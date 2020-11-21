4ac7daa8e172bd691685e255719dd96f4c14f22d
Joaquin Moreno <jmavs21@gmail.com>
2020-11-20 21:06:12 -0600

    Add latest libs for firebase and refactor.

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
  * Maze Game.
  */
 (() => {
-  if (typeof firebase === 'undefined') {
-    document.getElementById('p1').innerHTML =
-      'Error: no connection to Firebase.';
-  }
+  if (typeof firebase === 'undefined')
+    document.getElementById('p1').innerHTML = 'No connection to Firebase!';
 
   const db = firebase.firestore();
 
@@ -16,7 +14,6 @@
   _canvas.width = document.getElementById('content').clientWidth;
   _canvas.height = _canvas.width;
   const _context = _canvas.getContext('2d');
-  const _canvasSize = _canvas.width;
   const _colorMaze = '#000000';
 
   let _player = null;
@@ -30,17 +27,10 @@
   let _graph;
   let _goal;
 
-  window.onload = async () => {
-    try {
-      await validateGameSettings();
-      await gameListener();
-    } catch (err) {
-      console.log('Error with firebase: ', err);
-    }
-  };
+  window.onload = async () => await startGameState();
 
-  window.onunload = window.onbeforeunload = async () => {
-    await deletePlayer(_idPlayer);
+  window.onunload = window.onbeforeunload = () => {
+    deletePlayer(_idPlayer);
     return null;
   };
 
@@ -49,27 +39,32 @@
   window.moveLeft = async () => await drawPlayerMove(-1, -_wallSize, false);
   window.moveRight = async () => await drawPlayerMove(1, _wallSize, false);
 
-  document.body.addEventListener('keydown', (event) => {
-    if (event.keyCode == 38 || event.keyCode == 87) {
-      // up, w
-      event.preventDefault();
-      window.moveUp();
-    } else if (event.keyCode == 37 || event.keyCode == 65) {
-      // left, a
-      event.preventDefault();
-      window.moveLeft();
-    } else if (event.keyCode == 40 || event.keyCode == 83) {
-      // down, s
-      event.preventDefault();
-      window.moveDown();
-    } else if (event.keyCode == 39 || event.keyCode == 68) {
-      // right, d
-      event.preventDefault();
-      window.moveRight();
+  document.body.addEventListener('keydown', async (e) => {
+    if (e.key === 'ArrowUp' || e.key === 'W' || e.key === 'w') {
+      e.preventDefault();
+      await window.moveUp();
+    } else if (e.key === 'ArrowLeft' || e.key === 'A' || e.key === 'a') {
+      e.preventDefault();
+      await window.moveLeft();
+    } else if (e.key === 'ArrowDown' || e.key === 'S' || e.key === 's') {
+      e.preventDefault();
+      await window.moveDown();
+    } else if (e.key === 'ArrowRight' || e.key === 'D' || e.key === 'd') {
+      e.preventDefault();
+      await window.moveRight();
     }
   });
 
-  const validateGameSettings = async () => {
+  window.addEventListener('unhandledrejection', (e) => {
+    console.error('Unhandled Firebase reject:', e.reason);
+  });
+
+  const startGameState = async () => {
+    await createSettingsIfNeeded();
+    createSettingsListener();
+  };
+
+  const createSettingsIfNeeded = async () => {
     const settingsRef = gameRef.doc('settings');
     const settings = await settingsRef.get();
     if (!settings.exists) {
@@ -82,24 +77,11 @@
     }
   };
 
-  const deletePlayer = async (id) => {
-    if (id !== null) await playersRef.doc(id).delete();
+  const createSettingsListener = () => {
+    gameRef.where('rows', '>=', 2).onSnapshot(setGameWithSettings);
   };
 
-  const deleteUnusedPlayers = () => {
-    setTimeout(async () => {
-      const players = await playersRef.get();
-      players.forEach(async (player) => {
-        if (player.data().r !== _rows) await deletePlayer(player.id);
-      });
-    }, 3000);
-  };
-
-  const gameListener = async () => {
-    gameRef.where('rows', '>=', 2).onSnapshot(updateSettings);
-  };
-
-  const updateSettings = async (settings) => {
+  const setGameWithSettings = async (settings) => {
     settings.forEach((setting) => {
       const data = setting.data();
       _startLevel = data.startLevel;
@@ -107,27 +89,15 @@
       _rows = data.rows;
       _seed = data.seed;
     });
-    restartGame();
-    updatePlayer();
-    deleteUnusedPlayers();
-  };
-
-  const playersListener = () => {
-    playersRef.where('x', '>=', 0).onSnapshot(createPlayers);
-  };
-
-  const createPlayers = (queryPlayers) => {
-    const players = [];
-    queryPlayers.forEach((player) => {
-      const data = player.data();
-      players.push(new Player(0, new Position(data.x, data.y), data.c));
-    });
-    drawPlayers(players);
+    drawGame();
+    await updatePlayer();
+    await deleteUnusedPlayers();
   };
 
   const updatePlayer = async () => {
     if (_idPlayer === null) {
       await addNewPlayer();
+      createPlayersListener();
     } else {
       await playersRef.doc(_idPlayer).update({
         x: _player.position.x,
@@ -135,13 +105,6 @@
         r: _rows,
       });
     }
-  };
-
-  const updateGameSettings = async () => {
-    await gameRef.doc('settings').update({
-      rows: _rows,
-      seed: _seed,
-    });
   };
 
   const addNewPlayer = async () => {
@@ -152,167 +115,33 @@
       r: _rows,
     });
     _idPlayer = newPlayerRef.id;
-    playersListener();
   };
 
-  const drawPlayers = (players) => {
-    drawMaze();
-    drawGoal(_goal.x, _goal.y);
-    for (let player of players) {
-      drawPlayer(player.position.x, player.position.y, player.color);
-    }
+  const createPlayersListener = () => {
+    playersRef.where('x', '>=', 0).onSnapshot(setPlayers);
   };
 
-  const restartGame = () => {
-    _wallSize = 1.0 / _rows;
-    _walls = createAllWalls();
-    shuffle(_walls);
-    _graph = createGraphFromNonWalls(removeNonWalls());
-    _player = createPlayerPosition();
-    _goal = new Position(
-      1.0 - _wallSize + _wallSize / 3.3,
-      1.0 - _wallSize + _wallSize / 3.3
-    );
-    drawMaze();
-    drawPlayer(_player.position.x, _player.position.y, _player.color);
-    drawGoal(_goal.x, _goal.y);
+  const setPlayers = (queryPlayers) => {
+    const players = [];
+    queryPlayers.forEach((player) => {
+      const data = player.data();
+      players.push(new Player(0, new Position(data.x, data.y), data.c));
+    });
+    drawPlayers(players);
   };
 
-  const createAllWalls = () => {
-    const numOfWalls = Math.floor((_rows - 1) * _rows * 2);
-    const walls = [];
-    const group = _rows - 1;
-    let first = 0;
-    const half = Math.floor(numOfWalls / 2);
-    let j = half;
-    let second = _rows;
-    for (let i = 0; i < half; i++) {
-      walls[i] = new Wall(first, ++first, true);
-      if (i % group === group - 1) first++;
-      walls[j++] = new Wall(i, second++, false);
-    }
-    return walls;
+  const deleteUnusedPlayers = async () => {
+    await waitForActiveGames();
+    const players = await playersRef.get();
+    players.docs.map((player) => {
+      if (player.data().r !== _rows) deletePlayer(player.id);
+    });
   };
 
-  const shuffle = (walls) => {
-    for (let i = 0; i < walls.length; i++) {
-      swap(i, getRandom(0, i), walls);
-    }
-  };
+  const waitForActiveGames = () => new Promise((res) => setTimeout(res, 3000));
 
-  const swap = (i, j, walls) => {
-    const tmp = walls[i];
-    walls[i] = walls[j];
-    walls[j] = tmp;
-  };
-
-  const getRandom = (min, max) => {
-    return Math.floor(pseudorandom() * (max - min + 1)) + min;
-  };
-
-  const pseudorandom = () => {
-    const x = Math.sin(_seed++) * 10000;
-    return x - Math.floor(x);
-  };
-
-  const removeNonWalls = () => {
-    const nonWalls = [];
-    const uf = new UnionFind(_rows * _rows);
-    for (let i = 0; i < _walls.length; i++) {
-      const wall = _walls[i];
-      if (!uf.connected(wall.v, wall.w)) {
-        uf.union(wall.v, wall.w);
-        nonWalls.push(wall);
-        _walls[i] = null;
-      }
-    }
-    const mazeWalls = [];
-    for (let i = 0; i < _walls.length; i++) {
-      if (_walls[i] !== null) {
-        mazeWalls.push(_walls[i]);
-      }
-    }
-    _walls = mazeWalls;
-    return nonWalls;
-  };
-
-  const createGraphFromNonWalls = (nonWalls) => {
-    const graph = new Graph(_rows * _rows);
-    for (const wall of nonWalls) {
-      graph.addEdge(wall.v, wall.w);
-      graph.addEdge(wall.w, wall.v);
-    }
-    return graph;
-  };
-
-  const createPlayerPosition = () => {
-    const x = Math.random() * (_wallSize / 2.0) + _wallSize / 4.0;
-    const y = Math.random() * (_wallSize / 2.0) + _wallSize / 4.0;
-    const color = _player === null ? getRandomColor() : _player.color;
-    return new Player(0, new Position(x, y), color);
-  };
-
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
-  const drawMaze = () => {
-    _context.clearRect(0, 0, _canvas.width, _canvas.height);
-    for (const wall of _walls) {
-      const v = wall.v;
-      const xWidth = _wallSize * (v % _rows);
-      const yHeight = Math.floor(v / _rows) * _wallSize + _wallSize;
-      if (wall.isVertical) {
-        drawLine(
-          xWidth + _wallSize,
-          yHeight,
-          xWidth + _wallSize,
-          yHeight - _wallSize
-        );
-      } else {
-        drawLine(xWidth, yHeight, xWidth + _wallSize, yHeight);
-      }
-    }
-    drawLine(0.0, 0.0, 1.0, 0.0); // bottom line
-    drawLine(0.0, 0.0, 0.0, 1.0); // left line
-    drawLine(1.0, 0.0, 1.0, 1.0); // right line
-    drawLine(0.0, 1.0, 1.0, 1.0); // upper line
-  };
-
-  const scale = (value) => {
-    return _canvasSize * value;
-  };
-
-  const drawLine = (x0, y0, x1, y1) => {
-    _context.beginPath();
-    _context.strokeStyle = _colorMaze;
-    _context.moveTo(scale(x0), scale(y0));
-    _context.lineTo(scale(x1), scale(y1));
-    _context.stroke();
-  };
-
-  const drawPlayer = (x, y, color) => {
-    _context.beginPath();
-    _context.fillStyle = color;
-    _context.arc(scale(x), scale(y), scale(_wallSize) / 5, 0, 2 * Math.PI);
-    _context.fill();
-  };
-
-  const drawGoal = (x, y) => {
-    _context.beginPath();
-    _context.fillStyle = _colorMaze;
-    _context.rect(
-      scale(x),
-      scale(y),
-      scale(_wallSize) / 2.5,
-      scale(_wallSize) / 2.5
-    );
-    _context.fill();
+  const deletePlayer = (id) => {
+    if (id !== null) playersRef.doc(id).delete();
   };
 
   const drawPlayerMove = async (toCell, wallWidth, isVerticalMove) => {
@@ -340,14 +169,183 @@
     }
   };
 
+  const updateGameSettings = async () => {
+    await gameRef.doc('settings').update({
+      rows: _rows,
+      seed: _seed,
+    });
+  };
+
+  const drawGame = () => {
+    _wallSize = 1.0 / _rows;
+    _walls = createAllWalls();
+    shuffle(_walls);
+    _graph = createGraphFromNonWalls(removeNonWalls());
+    _player = createPlayerPosition();
+    _goal = new Position(
+      1.0 - _wallSize + _wallSize / 3.3,
+      1.0 - _wallSize + _wallSize / 3.3
+    );
+    drawMaze();
+    drawPlayer(_player.position.x, _player.position.y, _player.color);
+    drawGoal(_goal.x, _goal.y);
+  };
+
+  const createAllWalls = () => {
+    const group = _rows - 1;
+    const numOfWalls = Math.floor(group * _rows * 2);
+    const walls = [];
+    let vertical = 0;
+    let horizontal = _rows;
+    const half = Math.floor(numOfWalls / 2);
+    for (let i = 0, j = half; i < half; i++) {
+      walls[i] = new Wall(vertical, ++vertical, true);
+      if (i % group === group - 1) vertical++;
+      walls[j++] = new Wall(i, horizontal++, false);
+    }
+    return walls;
+  };
+
+  const removeNonWalls = () => {
+    const nonWalls = [];
+    const uf = new UnionFind(_rows * _rows);
+    for (let i = 0; i < _walls.length; i++) {
+      const wall = _walls[i];
+      if (!uf.connected(wall.v, wall.w)) {
+        uf.union(wall.v, wall.w);
+        nonWalls.push(wall);
+        _walls[i] = null;
+      }
+    }
+    const mazeWalls = [];
+    for (let i = 0; i < _walls.length; i++) {
+      if (_walls[i] !== null) {
+        mazeWalls.push(_walls[i]);
+      }
+    }
+    _walls = mazeWalls;
+    return nonWalls;
+  };
+
+  const createGraphFromNonWalls = (nonWalls) => {
+    const graph = new Graph(_rows * _rows);
+    for (const wall of nonWalls) {
+      graph.addEdge(wall.v, wall.w);
+    }
+    return graph;
+  };
+
+  const createPlayerPosition = () => {
+    const x = Math.random() * (_wallSize / 2.0) + _wallSize / 4.0;
+    const y = Math.random() * (_wallSize / 2.0) + _wallSize / 4.0;
+    const color = _player === null ? getRandomColor() : _player.color;
+    return new Player(0, new Position(x, y), color);
+  };
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const drawMaze = () => {
+    _context.clearRect(0, 0, _canvas.width, _canvas.width);
+    for (const wall of _walls) {
+      const v = wall.v;
+      const xWidth = _wallSize * (v % _rows);
+      const yHeight = Math.floor(v / _rows) * _wallSize + _wallSize;
+      if (wall.isVertical) {
+        drawLine(
+          xWidth + _wallSize,
+          yHeight,
+          xWidth + _wallSize,
+          yHeight - _wallSize
+        );
+      } else {
+        drawLine(xWidth, yHeight, xWidth + _wallSize, yHeight);
+      }
+    }
+    drawLine(0.0, 0.0, 1.0, 0.0); // bottom line
+    drawLine(0.0, 0.0, 0.0, 1.0); // left line
+    drawLine(1.0, 0.0, 1.0, 1.0); // right line
+    drawLine(0.0, 1.0, 1.0, 1.0); // upper line
+  };
+
+  const drawLine = (x0, y0, x1, y1) => {
+    _context.beginPath();
+    _context.strokeStyle = _colorMaze;
+    _context.moveTo(scale(x0), scale(y0));
+    _context.lineTo(scale(x1), scale(y1));
+    _context.stroke();
+  };
+
+  const scale = (value) => {
+    return _canvas.width * value;
+  };
+
+  const drawPlayers = (players) => {
+    drawMaze();
+    drawGoal(_goal.x, _goal.y);
+    for (let player of players) {
+      drawPlayer(player.position.x, player.position.y, player.color);
+    }
+  };
+
+  const drawPlayer = (x, y, color) => {
+    _context.beginPath();
+    _context.fillStyle = color;
+    _context.arc(scale(x), scale(y), scale(_wallSize) / 5, 0, 2 * Math.PI);
+    _context.fill();
+  };
+
+  const drawGoal = (x, y) => {
+    _context.beginPath();
+    _context.fillStyle = _colorMaze;
+    _context.rect(
+      scale(x),
+      scale(y),
+      scale(_wallSize) / 2.5,
+      scale(_wallSize) / 2.5
+    );
+    _context.fill();
+  };
+
+  const shuffle = (walls) => {
+    for (let i = 0; i < walls.length; i++) {
+      swap(i, getRandom(0, i), walls);
+    }
+  };
+
+  const swap = (i, j, walls) => {
+    const tmp = walls[i];
+    walls[i] = walls[j];
+    walls[j] = tmp;
+  };
+
+  const getRandom = (min, max) => {
+    return Math.floor(pseudorandom() * (max - min + 1)) + min;
+  };
+
+  const pseudorandom = () => {
+    const x = Math.sin(_seed++) * 10000;
+    return x - Math.floor(x);
+  };
+
   class Wall {
     constructor(v, w, isVertical) {
       this.v = v;
       this.w = w;
       this.isVertical = isVertical;
     }
-    toString() {
-      return this.v + '-' + this.w;
+  }
+
+  class Position {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
     }
   }
 
@@ -357,60 +355,22 @@
       this.position = position;
       this.color = color;
     }
-    toString() {
-      return this.v + ',' + this.position + ',' + this.color;
-    }
-  }
-
-  class Position {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
-    toString() {
-      return '(' + this.x + ',' + this.y + ')';
-    }
   }
 
   class Graph {
     constructor(V) {
-      this.adj = [];
       this.V = V;
-      this.E = 0;
+      this.adj = [];
       for (let v = 0; v < this.V; v++) {
         this.adj[v] = new Set();
       }
     }
     addEdge(v, w) {
-      this.validateVertex(v);
-      this.validateVertex(w);
       this.adj[v].add(w);
-      this.E++;
+      this.adj[w].add(v);
     }
     adjacents(v) {
-      this.validateVertex(v);
       return this.adj[v];
-    }
-    validateVertex(v) {
-      if (v < 0 || v >= this.V)
-        throw (
-          'IllegalArgumentException: vertex ' +
-          v +
-          ' is not between 0 and ' +
-          (this.V - 1)
-        );
-    }
-    toString() {
-      const s = [];
-      s.push(this.V + ' vertices, ' + this.E + ' edges \n');
-      for (let v = 0; v < this.V; v++) {
-        s.push(v + ': ');
-        for (let w of this.adj[v]) {
-          s.push(w + ' ');
-        }
-        s.push('\n');
-      }
-      return s.join('');
     }
   }
 
@@ -443,15 +403,6 @@
         this.parent[qP] = pP;
         this.rank[pP]++;
       }
-    }
-    validate(p) {
-      if (p < 0 || p >= this.parent.length)
-        throw (
-          'IllegalArgumentException Index ' +
-          p +
-          ' is not between 0 and ' +
-          (this.parent.length - 1)
-        );
     }
   }
 })();
